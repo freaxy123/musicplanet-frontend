7b2bfbbc18bd708dd3b4342fe61162fa57bfd122
Freek van Lier <freek.vlier@gmail.com>
2021-05-30 23:31:31 +0200

    First Push

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,14 +1,10 @@
 <template lang="">
     <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
-        <!-- Username Input -->
-        <div class="mb-4">
-            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username" type="text" placeholder="Username">
-        </div>
 
-        <!-- Password Input -->
-        <div class="mb-6">
-            <input class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="Password">
-            <!-- <p class="text-red text-xs italic">Please choose a password.</p> -->
+        <div class="m-4 space-y-4">
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username" type="text" placeholder="Username" v-model="username">
+
+            <input class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="Password" v-model="password">
         </div>
 
         <!-- Buttons -->
